360525fae562152d11b659ca273947b46fd8f720
Andreiarib <99724654+Andreiarib@users.noreply.github.com>
2026-05-19 15:31:46 -0300

    feat: adiciona dados do candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,49 @@
+// Criar o perfil do candidato
+
+const candidato = {
+  nome: "Amanda",
+  area: "Front-End",
+  habilidades: ["HTML", "CSS", "JavaScript", "React"],
+  experienciaMeses: "6 meses",
+};
+
+// Teste temporário para ver no console depois
+//console.log("Candidato cadastrado:", candidato.nome);
+//console.log("Habilidades do candidato:", candidato.habilidades.join(", "));
+
+// Criar um array lista de vagas
+
+const vagas = [
+  {
+    id: 1,
+    empresa: "Microsoft",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "Vue", "Lógica de Programação", "Scrum"],
+    salario: 3200,
+    modalidade: "Híbrido",
+  },
+  {
+    id: 2,
+    empresa: "TechGold",
+    cargo: "Front-End Trainee",
+    requisitos: ["HTML", "CSS", "JavaScript"],
+    salario: 2100,
+    modalidade: "Presencial",
+  },
+  {
+    id: 3,
+    empresa: "WebDigitalSolutions",
+    cargo: "Estágio JavaScript Júnior",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+    salario: 1950,
+    modalidade: "Presencial",
+  },
+  {
+    id: 4,
+    empresa: "NovaDigital",
+    cargo: "Desenvolvedor Vue.js",
+    requisitos: ["Vue", "Vuex", "Axios", "SCRUM"],
+    salario: 5290,
+    modalidade: "Remoto",
+  },
+];
